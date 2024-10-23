25f16bf37bf04f14b6239be18e4fa951bb8bfe86
weinux-cn <sua580er@gmail.com>
2024-10-23 15:32:14 +0800

    Update action.js
    Signed-off-by: weinux-cn <sua580er@gmail.com>

--- action.js
+++ action.js
@@ -1,24 +1,27 @@
 const core = require('@actions/core');
+const { spawn } = require('child_process');
+const path = require('path');
 
 function run() {
   try {
-    // This is just a thin wrapper around bash
-    const script = require('path').resolve(__dirname, 'script.sh');
+    // 解析脚本路径
+    const script = path.resolve(__dirname, 'script.sh');
+    console.log(`Running script: ${script}`);
 
-    console.log(script);
-    var child = require('child_process').execFile(script);
-    child.stdout.on('data', (data) => {
-      console.log(data.toString());
+    // 使用 spawn 而不是 execFile，捕获标准输出和错误输出
+    const child = spawn(script, [], {
+      stdio: 'inherit'  // 继承主进程的标准输入输出
     });
 
+    // 监听子进程关闭事件，输出退出码
     child.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
+      console.log(`Child process exited with code ${code}`);
       process.exit(code);
     });
-  }
-  catch (error) {
+
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
